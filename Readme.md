e087bfebb1740be113d94f30988f6396ddd492f1
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-07-20 02:02:28 -0400

    Partially functional snapshot. Need to bugfix everywhere.

--- Readme.md
+++ Readme.md
@@ -30,13 +30,6 @@ OR
 
 py2applet pylog.py index.tpl diary.sqlite3 icon.icns
 
-
-
-TODOS:
-
-1. Incorporate snippet in search and return search results like a search
-engine, from which we can click and open individual entries
-
-2. Look into full text search
-
-3. Bug in FF5 with textbox and span and submit button etc.
+TODO
+====
+- ( ) Fix new line issue (probably \r\n windows/dos issue) in edit box
--- jabda.py
+++ jabda.py
@@ -12,7 +12,7 @@ A - list box that lets us flip through our entries/search results
 B - reading/writing pane.
 
 New entry   -  Press 'n'. The B window will clear, allowing you to create your entry.
-               Press <shift> + <enter> to save the entry. ESC to cancel
+               Press <CMD> (<ctrl>) + <s> to save the entry. ESC to cancel
                Pane A is inactive during editing
 
 Edit entry  -  Just edit the text in box B.
@@ -34,6 +34,7 @@ class App(object):
     self.root = tki.Tk()
     self.root.wm_title('Jabda')
     self.load_prefs()
+    self.init_vars()
     self.connect_to_database()
     self.setup_window()
     self.set_entry_list(self.get_all_entries())
@@ -49,11 +50,9 @@ class App(object):
     self.config = ConfigParser.ConfigParser(self.config_default)
     self.config.read(self.config_fname)
 
-  def create_database(self):
-    logger.info('Creating new database')
-    c = self.conn.cursor()
-    c.execute('CREATE TABLE "entries" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "entry_date" datetime, "entry" text)')
-    self.conn.commit()
+  def init_vars(self):
+    self.entries = []
+    self.current_entry_id = None
 
   def connect_to_database(self):
     """Returns us a cursor and connection object to our database."""
@@ -61,7 +60,12 @@ class App(object):
     need_to_create_db = not os.path.exists(self.config.get('DEFAULT','dbpath'))
     self.conn = sqlite3.connect(self.config.get('DEFAULT','dbpath'))
     if need_to_create_db: self.create_database()
-    #self.conn.row_factory = sqlite3.Row
+
+  def create_database(self):
+    logger.info('Creating new database')
+    c = self.conn.cursor()
+    c.execute('CREATE TABLE "entries" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "entry_date" datetime, "entry" text)')
+    self.conn.commit()
 
   def get_all_entries(self):
     """Date formatting from http://www.sqlite.org/lang_datefunc.html."""
@@ -70,29 +74,29 @@ class App(object):
     return c.fetchall()
 
   def set_entry_list(self, list):
+    if len(list) == 0: return
     self.listbox.delete(0, tki.END) # clear
-    for id, value in list:
+    set_sel = 0
+    for n, (id, value) in enumerate(list):
       self.listbox.insert(tki.END, value)
+      if id == self.current_entry_id: set_sel = n
     self.entries = list
-    self.current_entry_id = None
+    self.listbox.activate(set_sel)
+    self.listbox.selection_set(set_sel)
 
   def setup_window(self):
     self.listbox = tki.Listbox(self.root, selectmode=tki.BROWSE, selectbackground='black', selectforeground='white', selectborderwidth=0, width=13, bd=5, highlightthickness=0)
     self.listbox.pack(side='left', fill='y')
-    #self.listbox.bind("<ButtonRelease-1>", self.selection_changed)
-    #self.listbox.bind("<KeyRelease-Up>", self.selection_changed)
-    #self.listbox.bind("<KeyRelease-Down>", self.selection_changed)
     self.listbox.bind('<<ListboxSelect>>', self.selection_changed)
     self.listbox.bind("<Key>", self.cmd_key_trap)
 
     self.edit_win = tki.Text(self.root, undo=True, width=50, height=12, fg='white', bg='black', insertbackground='white', highlightthickness=0, wrap=tki.WORD)
     self.edit_win.pack(side='left', expand=True, fill='both')
-    self.edit_win.bind('<<Modified>>', self.edit_start) #We set this rightaway because we want to be able to start a new entry by just typing
-    self.edit_win.bind("<Shift-Return>", self.shift_enter_pressed)
+    self.edit_win.bind('<<Modified>>', self.start_editing) #We set this rightaway because we want to be able to start a new entry by just typing in the empty box
+    self.edit_win.bind("<Mod1-s>", self.execute)
 
     geom=self.config.get('DEFAULT', 'geometry')
-    if geom != 'none':
-      self.root.geometry(geom)
+    if geom != 'none': self.root.geometry(geom)
 
   def cleanup_on_exit(self):
     """Save pending edits configuration."""
@@ -103,6 +107,7 @@ class App(object):
     self.root.quit() #Allow the rest of the quit process to continue
 
   def cmd_key_trap(self, event):
+    logger.debug(self.cmd_state)
     chr = event.char
     if self.cmd_state == 'Idle':
       if chr=='n':
@@ -115,6 +120,7 @@ class App(object):
         self.set_database()
 
   def selection_changed(self, event=None):
+    if len(self.entries) == 0: return
     item = self.listbox.curselection()[0]
     sel_id = int(self.entries[int(item)][0])
     c = self.conn.cursor()
@@ -128,32 +134,26 @@ class App(object):
     self.current_entry_id = entry[0]
 
   def new(self):
+    logger.debug('Starting new entry')
     self.current_entry_id = None #No id
     self.edit_win.unbind('<<Modified>>') #Deleting and inserting will trigger the callback
     self.edit_win.delete(1.0, tki.END)
     self.edit_win.focus_set()
     self.start_editing()
 
-  def edit_start(self, event):
-    self.start_editing()
-
-  def start_editing(self):
-    """We need to
-       1) Disable the listbox
-       2) Disconnect the selection changed function (Tkinter bug IMO)
-    """
+  def start_editing(self, event=None):
     self.cmd_state ='editing'
     self.listbox.config(state=tki.DISABLED)
     self.listbox.unbind('<<ListboxSelect>>') #Don't forget to bind after all this is over
 
   def end_editing(self):
     self.listbox.config(state=tki.NORMAL)
-    #TODO keep cursor at current entry
     self.set_entry_list(self.get_all_entries())
     self.listbox.focus_set()
     self.listbox.bind('<<ListboxSelect>>', self.selection_changed)
+    self.cmd_state = 'Idle'
 
-  def shift_enter_pressed(self, event):
+  def execute(self, event):
     if self.cmd_state=='editing':
       self.save()
       return 'break'
@@ -174,6 +174,9 @@ class App(object):
       c = self.conn.cursor()
       c.execute(query, bindings)
       self.conn.commit()
+      self.edit_win.unbind('<<Modified>>') #Deleting and inserting will trigger the callback
+      self.edit_win.edit_modified(0) #Deleting and inserting sets this flag
+      self.edit_win.bind('<<Modified>>', self.start_editing)
     self.end_editing()
 
   def log_command(self, cmd):
