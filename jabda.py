e485d7a79d3d037d246080b5bf388f5e3e181ffb
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-07-25 10:24:23 -0400

    Last commit before trying out simpler interface

--- jabda.py
+++ jabda.py
@@ -1,12 +1,13 @@
 """
 Jabda is a simple personal diary program. I am encouraged to write a diary if I can make entries quickly. I had Jabda as a bottle application using a browser as the GUI and that worked fine, but the program was not as responsive as I wanted. Jabda is now a very simple TKinter application running off a sqlite backend.
 
-|-------------------|
+---------------------
+|     |             |
 |     |             |
 |  A  |      B      |
 |     |             |
 |     |             |
-|-------------------|
+---------------------
 
 A - list box that lets us flip through our entries/search results
 B - reading/writing pane.
@@ -16,17 +17,21 @@ New entry   -  Press 'n'. The B window will clear, allowing you to create your e
                Pane A is inactive during editing
 
 Edit entry  -  Just edit the text in box B.
-               Press <shift> + <enter> to save. ESC to cancel
+               Press <CMD> (<ctrl>) + <s> to save. ESC to cancel
                Pane A is inactive during editing
 
-Search      -  Press 's'. The B window will clear. Type your term and hit <shift> + <enter>
+You can not quit in the middle of an edit. You need to save or hit ESC to cancel, before quitting
 
+Select database - Press 'd'.
+Create database - Press 'c'.
+
+Search      -  Press 's'. The B window will clear. Type your term and press <CMD> + <s>
 Exit search -  Press 'x'
 """
 
 import logging
 logger = logging.getLogger(__name__)
-import Tkinter as tki, argparse, ConfigParser, sqlite3, os.path, datetime
+import Tkinter as tki, tkFileDialog, argparse, ConfigParser, sqlite3, os.path, datetime
 
 class App(object):
 
@@ -35,8 +40,8 @@ class App(object):
     self.root.wm_title('Jabda')
     self.load_prefs()
     self.init_vars()
-    self.connect_to_database()
     self.setup_window()
+    self.connect_to_database()
     self.set_entry_list(self.get_all_entries())
     self.root.wm_protocol("WM_DELETE_WINDOW", self.cleanup_on_exit)
     self.cmd_state = 'Idle'
@@ -54,21 +59,39 @@ class App(object):
     self.entries = []
     self.current_entry_id = None
 
-  def connect_to_database(self):
-    """Returns us a cursor and connection object to our database."""
+  def setup_window(self):
+    self.listbox = tki.Listbox(self.root, selectmode=tki.BROWSE,
+                               selectbackground='black', selectforeground='white',
+                               selectborderwidth=0, width=13, bd=5, highlightthickness=0)
+    self.listbox.pack(side='left', fill='y')
+    self.listbox.bind('<<ListboxSelect>>', self.selection_changed)
+    self.listbox.bind("<Key>", self.cmd_key_trap)
+
+    self.edit_win = tki.Text(self.root, undo=True, width=50, height=12,
+                             fg='white', bg='black', insertbackground='white', highlightthickness=0, wrap=tki.WORD)
+    self.edit_win.pack(side='right', expand=True, fill='both')
+    self.edit_win.bind('<<Modified>>', self.start_editing)
+    #We set this rightaway because we want to be able to start a new entry by just typing in the empty box
+    self.edit_win.bind("<Mod1-s>", self.execute)
+    self.edit_win.bind("<Escape>", self.end_editing)
+
+    geom=self.config.get('DEFAULT', 'geometry')
+    if geom != 'none': self.root.geometry(geom)
+
+  def connect_to_database(self, force_create=False):
     logger.info('Connecting to {:s}'.format(self.config.get('DEFAULT','dbpath')))
-    need_to_create_db = not os.path.exists(self.config.get('DEFAULT','dbpath'))
+    need_to_create_db = not os.path.exists(self.config.get('DEFAULT','dbpath')) or force_create
     self.conn = sqlite3.connect(self.config.get('DEFAULT','dbpath'))
     if need_to_create_db: self.create_database()
 
   def create_database(self):
     logger.info('Creating new database')
     c = self.conn.cursor()
+    c.execute('DROP TABLE IF EXISTS entries')
     c.execute('CREATE TABLE "entries" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "entry_date" datetime, "entry" text)')
     self.conn.commit()
 
   def get_all_entries(self):
-    """Date formatting from http://www.sqlite.org/lang_datefunc.html."""
     c = self.conn.cursor()
     c.execute("SELECT id, DATE(entry_date) FROM entries order by entry_date desc")
     return c.fetchall()
@@ -84,23 +107,8 @@ class App(object):
     self.listbox.activate(set_sel)
     self.listbox.selection_set(set_sel)
 
-  def setup_window(self):
-    self.listbox = tki.Listbox(self.root, selectmode=tki.BROWSE, selectbackground='black', selectforeground='white', selectborderwidth=0, width=13, bd=5, highlightthickness=0)
-    self.listbox.pack(side='left', fill='y')
-    self.listbox.bind('<<ListboxSelect>>', self.selection_changed)
-    self.listbox.bind("<Key>", self.cmd_key_trap)
-
-    self.edit_win = tki.Text(self.root, undo=True, width=50, height=12, fg='white', bg='black', insertbackground='white', highlightthickness=0, wrap=tki.WORD)
-    self.edit_win.pack(side='left', expand=True, fill='both')
-    self.edit_win.bind('<<Modified>>', self.start_editing) #We set this rightaway because we want to be able to start a new entry by just typing in the empty box
-    self.edit_win.bind("<Mod1-s>", self.execute)
-
-    geom=self.config.get('DEFAULT', 'geometry')
-    if geom != 'none': self.root.geometry(geom)
-
   def cleanup_on_exit(self):
-    """Save pending edits configuration."""
-    #self.save_entry()
+    if self.cmd_state == 'editing': return
     self.config.set('DEFAULT', 'geometry', self.root.geometry())
     with open(self.config_fname, 'wb') as configfile:
       self.config.write(configfile)
@@ -117,7 +125,9 @@ class App(object):
         self.start_search()
         return 'break'
       elif chr=='d':
-        self.set_database()
+        self.switch_database()
+      elif chr=='c':
+        self.new_database()
 
   def selection_changed(self, event=None):
     if len(self.entries) == 0: return
@@ -130,7 +140,7 @@ class App(object):
     self.edit_win.delete(1.0, tki.END)
     self.edit_win.insert(tki.END, entry[1])
     self.edit_win.edit_modified(0) #Deleting and inserting sets this flag
-    self.edit_win.bind('<<Modified>>', self.edit_start)
+    self.edit_win.bind('<<Modified>>', self.start_editing)
     self.current_entry_id = entry[0]
 
   def new(self):
@@ -146,7 +156,7 @@ class App(object):
     self.listbox.config(state=tki.DISABLED)
     self.listbox.unbind('<<ListboxSelect>>') #Don't forget to bind after all this is over
 
-  def end_editing(self):
+  def end_editing(self, event=None):
     self.listbox.config(state=tki.NORMAL)
     self.set_entry_list(self.get_all_entries())
     self.listbox.focus_set()
@@ -164,7 +174,7 @@ class App(object):
   def save(self):
     if self.edit_win.edit_modified():
       logger.debug('Saving entry')
-      entry_text = self.edit_win.get(1.0, tki.END)
+      entry_text = self.edit_win.get(1.0, tki.END).rstrip()
       if self.current_entry_id is None:
         query = 'INSERT INTO entries (entry_date, entry) VALUES (?,?)'
         bindings = (datetime.datetime.now(), entry_text)
@@ -174,10 +184,25 @@ class App(object):
       c = self.conn.cursor()
       c.execute(query, bindings)
       self.conn.commit()
+      if self.current_entry_id is None: self.current_entry_id = c.lastrowid
       self.edit_win.unbind('<<Modified>>') #Deleting and inserting will trigger the callback
       self.edit_win.edit_modified(0) #Deleting and inserting sets this flag
       self.edit_win.bind('<<Modified>>', self.start_editing)
     self.end_editing()
+
+  def switch_database(self):
+    filename = tkFileDialog.askopenfilename()
+    if filename == '': return
+    self.config.set('DEFAULT','dbpath', filename)
+    self.connect_to_database()
+    self.set_entry_list(self.get_all_entries())
+
+  def new_database(self):
+    filename = tkFileDialog.asksaveasfilename()
+    if filename == '': return
+    self.config.set('DEFAULT','dbpath', filename)
+    self.connect_to_database(force_create=True)
+    self.set_entry_list(self.get_all_entries())
 
   def log_command(self, cmd):
     if hasattr(self, 'log_win_after_id'):
@@ -187,11 +212,6 @@ class App(object):
 
   def clear_log_command(self):
     self.log_win.delete(1.0, tki.END)
-
-  def command_cancel(self):
-    self.cmd_win.delete(1.0, tki.END)
-    self.cmd_state = 'Idle'
-    self.log_command('Command canceled.')
 
   def search_execute(self, query_str):
     self.log_command('Searching for {:s}'.format(lch.query_to_rawquery(query_str)))
